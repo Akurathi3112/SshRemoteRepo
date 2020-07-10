8acfa6fe734961dd0f07f43ea2f985800bea3dd1
Rakesh Akurathi <akurthi.rakesh9@gmail.com>
2020-07-10 16:34:45 -0400

    I am pushing all the files to my GitHub

---
+++ C_Program.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+printf("Hello! I am C-program. Thank you");
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,5 @@
+var texts = "I am your Javascript Program";
+
+console.log(texts);
+
+
